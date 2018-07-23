c6159d1e68ca037fdc317a0958d4cd73b268b35e
Yu JiSang <zxuz34@gmail.com>
2018-07-23 14:32:33 +0900

    test

--- src/main/java/com/CatchJob/controller/MainController.java
+++ src/main/java/com/CatchJob/controller/MainController.java
@@ -1,15 +1,23 @@
 package com.CatchJob.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.CatchJob.dao.TestDao;
+
 @Controller
 public class MainController {
-
+	
+	@Autowired
+	TestDao testDao;
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index() {
 		System.out.println("메인화면 접속");
+		
+		System.out.println(testDao.selectOne());
 		return "index";
 	}
 
---
+++ src/main/java/com/CatchJob/dao/DaoTest.java
@@ -0,0 +1,22 @@
+package com.CatchJob.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+public class DaoTest {
+	
+	@Autowired
+	TestDao dao;
+	
+	@Test
+	public void testDao() {
+		System.out.println("123123");
+		dao.selectOne();
+		System.out.println("345345");
+	}
+}
