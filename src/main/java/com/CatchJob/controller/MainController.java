56bf14f9cb2ca36bae128db7441a597d55098e66
Yu JiSang <zxuz34@gmail.com>
2018-08-01 13:50:47 +0900

    test

--- src/main/java/com/CatchJob/controller/MainController.java
+++ src/main/java/com/CatchJob/controller/MainController.java
@@ -1,15 +1,23 @@
 package com.CatchJob.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.CatchJob.service.SalaryServiceImp;
+
 @Controller
 public class MainController {
+	@Autowired
+	SalaryServiceImp svc;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
 		System.out.println("메인화면 접속");
+		
+		svc.getRankList();
+		
 		
 		return "main";
 	}
