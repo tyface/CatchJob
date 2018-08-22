b8020717e63f2467e726d9f10795e9f2a1f2e466
inyoung2 <hrsiy0126@gmail.com>
2018-08-22 12:57:11 +0900

    before marge - reviewdao

--- src/main/java/com/CatchJob/dao/ReviewDao.java
+++ src/main/java/com/CatchJob/dao/ReviewDao.java
@@ -36,11 +36,11 @@ public interface ReviewDao {
 	public List<Map<String, String>> question(int entIndex);
 	
 	//리뷰 보기	
-	public List<Review> reviewList(int entIndex);
+//	public List<Review> reviewList(int entIndex);
 	//수정 삭제 페이지에 리뷰 띄우기	
-	public List<Review> reviewListByMember(int memberIndex);
+	public List<Review> reviewListByMember(Map<String, String> map);
 	//수정 할 데이터 가져오기
-	public Review review(Map<String, String> data);
+	public Review selectOneReview(Map<String, String> data);
 	
 	/*//질문별로 리뷰 보기
 	public List<Review> reviewListByQNum(Map<String, String> data);*/
